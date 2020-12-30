ecd2721cd69487f4e6bb8fa45dd32cfca6376642
beerwithstraw <agarwalp@tcd.ie>
2020-12-30 21:38:53 +0000

    NavBar LinkedIn

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search'
+import HomeIcon from '@material-ui/icons/Home'
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount'
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter'
+import ChatIcon from '@material-ui/icons/Chat'
+import NotificationsIcon from '@material-ui/icons/Notifications'
+import HeaderOption from './HeaderOption';
+
 
 function Header() {
     return (
         <div className="header">
-            <h2>This is Header</h2>
-
             <div className="header__left">
-                <img src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" alt=""/>
+                <img src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" alt="Logo"/>
                 <div className="header__search">
                     <SearchIcon />
                     <input type="text"/>
@@ -16,7 +21,13 @@ function Header() {
             </div>
 
             <div className="header__right">
-
+                <HeaderOption Icon={HomeIcon} title="Home"/>
+                <HeaderOption Icon={SupervisorAccountIcon} title="My Network"/>
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
+                <HeaderOption Icon={ChatIcon} title="Messaging"/>
+                <HeaderOption Icon={NotificationsIcon} title="Notifications"/>
+                <HeaderOption avatar="me.jpg" title="Me"/>
+ 
             </div>
             
         </div>
