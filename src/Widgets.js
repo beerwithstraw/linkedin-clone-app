1d44fb9e1be32fc1490b15401ceb429140dde68f
beerwithstraw <agarwalp@tcd.ie>
2021-01-04 01:44:53 +0000

    widgets

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import { FiberManualRecord, Info } from '@material-ui/icons'
+
+import './Widgets.css'
 
 function Widgets() {
-    return (
-        <div>
-            
+    const newsArticle = (heading, subtitle) => (
+        <div className="widgets__article">
+            <div className="widgets__articleLeft">
+                <FiberManualRecord />
+            </div>
+
+            <div className="widgets__articleRight">
+                <h4>{heading}</h4>
+                <p>{subtitle}</p>
+            </div>
         </div>
+    )
+
+    return (
+        <div className="widgets">
+            <div className="widgets__header">
+                <h2>LinkedIn News</h2>
+                <Info />
+            </div>
+            {newsArticle("Coronavirus UK Updates", "Top News")}
+
+        </div>
+        
     )
 }
 
